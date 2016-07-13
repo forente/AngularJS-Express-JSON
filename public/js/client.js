d261a806e10722b95ca0664a4b424257d24ef9cd
forente <zerofox16@gmail.com>
2016-07-13 09:21:52 -0500

    added athletes

--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,26 @@
 
+var app = angular.module('olympiean',[]);
+
+app.controller('athlete',function($scope, $http){
+
+
+  var getBoxers ={
+    method: 'GET',
+    url: '/boxing'
+  };
+
+  function successHandle(response) {
+  $scope.athlete = response.data;
+  console.log('Success:', response);
+  }
+  function faliureHandle(response) {
+
+  console.log('Failure:', response);
+  }
+
+  $scope.getAthlete  = function getAthlete(){
+    $http(getBoxers).then(successHandle, faliureHandle);
+  }
+
+
+});
